99938f9c1c254a673c7c9b4e5dfe48f4bcad348d
Ivan Vanderbyl <ivanvanderbyl@me.com>
2013-02-16 22:28:05 +1000

    Require app directories

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,5 +3,10 @@
 //= require ember
 //= require ember-data
 //= require_self
+//= require_tree ./controllers
+//= require_tree ./models
+//= require_tree ./views
+//= require_tree ./templates
+//= require_tree ./routes
 
 console.log("Loaded")
